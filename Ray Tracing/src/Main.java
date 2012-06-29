1200291fb727f40d8b10245e5be4799b4c15ef90
Jonathan Bishop <jbishop.rwc@gmail.com>
2012-06-29 10:02:37 -0700

    Test of small change

--- Ray Tracing/src/Main.java
+++ Ray Tracing/src/Main.java
@@ -9,7 +9,6 @@ public class Main {
 		scene.add(new Plane(new Vector(0,0,0), new Vector(-1,0,0), Color.WHITE));
 		scene.add(new PointLight(new Vector(-.2,.2,0), Color.GRAY));
 		scene.add(new PointLight(new Vector(-.2,-.2,0), Color.GRAY));
-		
 		Color color = new Color(0,0,0);
 		RGBImage i = new RGBImage(1000, 1000);
 		for (int row = 0; row < 1000; row++) {
