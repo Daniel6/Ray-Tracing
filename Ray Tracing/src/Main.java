494c1b35fb21663688e90db91df929e060ee8c10
Jonathan Bishop <jbishop.rwc@gmail.com>
2012-06-29 13:40:59 -0700

    Changes to scene in main.

--- Ray Tracing/src/Main.java
+++ Ray Tracing/src/Main.java
@@ -1,17 +1,16 @@
 
 public class Main {
 	public static void main(String args[]) {
-		Vector viewpoint = new Vector(2, 2, 2);
+		Vector viewpoint = new Vector(4, 4, 4);
 		Vector lookat = new Vector(0, 0, 0);
-		Camera camera = new Camera(viewpoint, lookat, 1001);
+		Camera camera = new Camera(viewpoint, lookat, 500);
 		Scene scene = new Scene();
 		scene.add(new Sphere(new Vector(0,0,0), 0.2, Color.WHITE));
-		scene.add(new Sphere(new Vector(1,0,0), 0.1, Color.RED));
-		scene.add(new Sphere(new Vector(0,1,0), 0.1, Color.GREEN));
-		scene.add(new Sphere(new Vector(0,0,1), 0.1, Color.BLUE));
-		scene.add(new Plane(new Vector(0,0,-1), new Vector(0,0,1), Color.WHITE));
-		scene.add(new PointLight(new Vector(0, 0, 10), Color.WHITE));
-//		scene.add(new PointLight(new Vector(-.2,-.2,0), Color.GRAY));
+		scene.add(new Sphere(new Vector(1,0,0), 0.2, Color.RED));
+		scene.add(new Sphere(new Vector(0,1,0), 0.2, Color.GREEN));
+		scene.add(new Sphere(new Vector(0,0,1), 0.2, Color.BLUE));
+		scene.add(new Plane(new Vector(0,0,0), new Vector(0,0,1), Color.WHITE));
+		scene.add(new PointLight(new Vector(0.5, 0, 0.5), Color.WHITE));
 		RGBImage image = camera.process(scene);
 		image.displayImage();
 	}
