612b5b7e9e41778a0f0b966b97241825d1ced8dd
Jonathan Bishop <jbishop.rwc@gmail.com>
2012-06-29 23:17:59 -0700

    Small change to Vector.

--- Ray Tracing/src/Vector.java
+++ Ray Tracing/src/Vector.java
@@ -1,54 +1,54 @@
 
 public class Vector {
-	double k_X;
-	double k_Y;
-	double k_Z;
+	double x;
+	double y;
+	double z;
 	public Vector(double X, double Y, double Z) {
-		k_X = X;
-		k_Y = Y;
-		k_Z = Z;
+		x = X;
+		y = Y;
+		z = Z;
 	}
 	public final double getX() {
-		return k_X;
+		return x;
 	}
 	public final double getY() {
-		return k_Y;
+		return y;
 	}
 	public final double getZ() {
-		return k_Z;
+		return z;
 	}
-	public void setX(double k_X) {
-		this.k_X = k_X;
+	public void setX(double X) {
+		this.x = X;
 	}
-	public void setY(double k_Y) {
-		this.k_Y = k_Y;
+	public void setY(double Y) {
+		this.y = Y;
 	}
-	public void setZ(double k_Z) {
-		this.k_Z = k_Z;
+	public void setZ(double Z) {
+		this.z = Z;
 	}
 	public Vector add(Vector v) {
-		return new Vector(k_X + v.k_X, k_Y + v.k_Y, k_Z + v.k_Z);
+		return new Vector(x + v.x, y + v.y, z + v.z);
 	}
 	public Vector sub(Vector v) {
-		return new Vector(k_X - v.k_X, k_Y - v.k_Y, k_Z - v.k_Z);
+		return new Vector(x - v.x, y - v.y, z - v.z);
 	}
 	public double length() {
-		return Math.sqrt(Math.pow(k_X,2.0)+Math.pow(k_Y,2.0)+Math.pow(k_Z,2.0));
+		return Math.sqrt(Math.pow(x,2.0)+Math.pow(y,2.0)+Math.pow(z,2.0));
 	}
 	public String toString() {
-		return "vector(" + k_X + "," + k_Y + "," + k_Z + ")";
+		return "vector(" + x + "," + y + "," + z + ")";
 	}
 	public double dot(Vector v) {
-		return (k_X * v.k_X) + (k_Y * v.k_Y) + (k_Z * v.k_Z);
+		return (x * v.x) + (y * v.y) + (z * v.z);
 	}
 	public Vector cross(Vector v) {
-		return new Vector((k_Y * v.k_Z) - (v.k_Y * k_Z), (k_Z * v.k_X) - (v.k_Z * k_X), (k_X * v.k_Y) - (v.k_X * k_Y));
+		return new Vector((y * v.z) - (v.y * z), (z * v.x) - (v.z * x), (x * v.y) - (v.x * y));
 	}
 	public Vector mult(double d) {
-		return new Vector(k_X * d, k_Y * d, k_Z * d);
+		return new Vector(x * d, y * d, z * d);
 	}
 	public Vector div(double d) {
-		return new Vector(k_X / d, k_Y / d, k_Z / d);
+		return new Vector(x / d, y / d, z / d);
 	}
 	public Vector norm() {
 		double l = this.length();
