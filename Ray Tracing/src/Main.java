f2b4bc88adc8c81f839ec126ec0486b627b8133c
Jonathan Bishop <jbishop.rwc@gmail.com>
2012-07-01 23:26:44 -0700

    Nice scene.

--- Ray Tracing/src/Main.java
+++ Ray Tracing/src/Main.java
@@ -4,15 +4,19 @@ public class Main {
 		Vector viewpoint = new Vector(10,5,5);
 		Vector lookat = new Vector(0, 0, 0);
 		Camera camera = new Camera(viewpoint, lookat, 1000);
-		camera.setZoom(4.0);
+		camera.setZoom(2.0);
 		Scene scene = new Scene();
-		scene.add(new Sphere(new Vector(0, 0,0.4), 0.4, new Material(Color.GRAY, Color.GRAY, Color.BLACK, 1)));
+		Color gray25 = new Color(0.25, 0.25, 0.25);
+		Color gray75 = new Color(0.75, 0.75, 0.75);
+		scene.add(new Sphere(new Vector(0, 0,0.4), 0.4, new Material(Color.BLACK, Color.WHITE, Color.BLACK, 1)));
 		scene.add(new Sphere(new Vector(1, 0,0.2), 0.2, new Material(Color.RED, Color.BLACK, Color.BLACK, 1)));
 		scene.add(new Sphere(new Vector(0, 1,0.2), 0.2, new Material(Color.GREEN, Color.BLACK, Color.BLACK, 1)));
 		scene.add(new Sphere(new Vector(0,-1,0.2), 0.2, new Material(Color.BLUE, Color.BLACK, Color.BLACK, 1)));
-		scene.add(new Plane(new Vector(0,0,0), new Vector(0,0,1), new Material(Color.GRAY, Color.GRAY, Color.BLACK, 1)));
-		scene.add(new PointLight(new Vector(1.0, 0, 0.5), Color.WHITE));
-		scene.add(new PointLight(new Vector(-0.5, .5, 0.5), Color.WHITE));
+		scene.add(new Sphere(new Vector(-1,0,0.2), 0.2, new Material(Color.YELLOW, Color.BLACK, Color.BLACK, 1)));
+		scene.add(new Plane(new Vector(0,0,0), new Vector(0,0,1), new Material(gray25, gray25, Color.BLACK, 1)));
+		scene.add(new Plane(new Vector(-2,0,0), new Vector(1,0,0), new Material(gray75, gray25, Color.BLACK, 1)));
+		scene.add(new PointLight(new Vector(2, 2, 2), gray75));
+		scene.add(new PointLight(new Vector(2, -2, 2), gray25));
 		RGBImage image = camera.process(scene);
 		image.displayImage();
 	}
