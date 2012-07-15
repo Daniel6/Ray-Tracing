b5f00c7b1bf5d4601af69640375554039a460283
Jonathan Bishop <jbishop.rwc@gmail.com>
2012-07-14 17:13:27 -0700

    Use floor roughness

--- Ray Tracing/src/Main.java
+++ Ray Tracing/src/Main.java
@@ -1,18 +1,20 @@
 
 public class Main {
 	public static void main(String args[]) throws InterruptedException {
-		Vector viewpoint = new Vector(10,5,5);
+		Vector viewpoint = new Vector(4,2,2);
 		Vector lookat = new Vector(0, 0, 0);
-		Camera camera = new Camera(viewpoint, lookat, 600, 800);
+		Camera camera = new Camera(viewpoint, lookat, 1080, 1920);
 		camera.setAntialias(0);
-		camera.setZoom(10);
+		camera.setZoom(4);
 		Scene scene = new Scene();
 		scene.add(new Sphere(new Vector(0, 0,0.2), 0.2, Material.plastic(Color.WHITE)));
 		scene.add(new Sphere(new Vector(.5, 0,0.2), 0.2, Material.plastic(Color.RED)));
 		scene.add(new Sphere(new Vector(0, .5,0.2), 0.2, Material.plastic(Color.GREEN)));
 		scene.add(new Sphere(new Vector(0,-.5,0.2), 0.2, Material.plastic(Color.BLUE)));
 		scene.add(new Sphere(new Vector(-.5,0,0.2), 0.2, Material.plastic(Color.YELLOW)));
-		scene.add(new Plane(new Vector(0,0,0), new Vector(0,0,1), Material.plastic(Color.gray(1.0))));
+		Material floor = Material.plastic(Color.WHITE);
+		floor.setRoughness(new Roughness(100, .1));
+		scene.add(new Plane(new Vector(0,0,0), new Vector(0,0,1), floor));
 		scene.add(new PointLight(new Vector(5,-5, 2), Color.gray(0.8)));
 		scene.add(new PointLight(new Vector(5, 5, 2), Color.gray(0.8)));
 		long t0 = System.currentTimeMillis();
