c0f09036375bcfee44ab64edeae11d20ec2136bb
Jonathan Bishop <jbishop.rwc@gmail.com>
2012-07-28 19:28:13 -0700

    Adjust entity tolerance

--- Ray Tracing/src/Entity.java
+++ Ray Tracing/src/Entity.java
@@ -3,5 +3,5 @@ public abstract class Entity {
 	public abstract Scene getScene();
 	public abstract void setScene(Scene s);
 	public abstract Intersection findIntersect(Ray r) throws Exception;
-	public double getTol() { return 1.0e-10; }
+	public double getTol() { return 1.0e-8; }
 }
--- Ray Tracing/src/Main.java
+++ Ray Tracing/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 		Vector viewpoint = new Vector(4,2,2);
 		Vector lookat = new Vector(0, 0, .1);
 		Camera camera = new Camera(viewpoint, lookat, 600, 800);
-		camera.setAntialias(2);
+		camera.setAntialias(0);
 		camera.setZoom(4);
 		Scene scene = new Scene();
 		Material globe = Material.metal(Color.WHITE);
@@ -14,8 +14,8 @@ public class Main {
 		scene.add(new Sphere(new Vector(0, .5,0.2), 0.2, Material.plastic(Color.GREEN)));
 		scene.add(new Sphere(new Vector(0,-.5,0.2), 0.2, Material.plastic(Color.BLUE)));
 		scene.add(new Sphere(new Vector(-.5,0,0.2), 0.2, Material.plastic(Color.YELLOW)));
-		scene.add(new ParallelPiped(new Vector(0.3, 0.3, 0), new Vector(0.3, 0, 0), new Vector(0, 0.5, 0), new Vector(0, 0, 0.1), Material.plastic(Color.WHITE)));
-		scene.add(new Cylinder(new Vector(0, 0, 0), new Vector(0, 0, .5), .3, Material.plastic(Color.WHITE)));
+		scene.add(new ParallelPiped(new Vector(0.3, 0.3, 0), new Vector(0.3, 0, 0), new Vector(0, 0.5, 0), new Vector(0, 0, 0.1), Material.plastic(Color.MAGENTA.mult(.5))));
+		scene.add(new Cylinder(new Vector(0, 0, 0), new Vector(0, 0, .5), .3, Material.metal(Color.WHITE)));
 		Material floor = Material.plastic(Color.WHITE);
 		floor.setRoughness(new Roughness(100, .01));
 		scene.add(new Plane(new Vector(0,0,0), new Vector(0,0,1), floor));
@@ -25,7 +25,7 @@ public class Main {
 		RGBImage image = camera.process(scene, 4);
 		long t1 = System.currentTimeMillis();
 		System.out.println("run time:" + (t1 - t0) / 1000.0 + "s");
-		image = image.magnify(2);
+		image = image.magnify(1);
 		image.displayImage();
 	}
 }
